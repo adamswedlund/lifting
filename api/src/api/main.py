f6fe9b60ea5987a095d263d4c0f6e1353d92a797
Adam Swedlund <aswedlund12@gmail.com>
2025-08-11 20:25:30 -0500

    Setup fastapi uvicorn server

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from api!")
+import fastapi
+import uvicorn
 
+app = fastapi.FastAPI()
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=8443)
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
